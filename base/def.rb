3b771862632e5dfe2e7265d41164050982cec777
github kadokichi <dydy2929@yahoo.co.jp>
2024-12-30 20:25:28 +0900

    method,fizzbuzz

---
+++ base/def.rb
@@ -0,0 +1,12 @@
+def greet(country)
+  return puts "countryを入力してください" if country.nil?
+  if country == "japan"
+    puts "こんにちは"
+  else
+    puts "hello"
+  end
+end
+
+greet("japan")
+greet("us")
+greet(nil)
